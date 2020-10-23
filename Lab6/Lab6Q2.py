4a219775ff4887e641444cad793841039389a4df
Paul <psilvestri9@gmail.com>
2020-10-23 13:54:06 -0400

    Fixed some issues with the lab6q2 code

--- Lab6/Lab6Q2.py
+++ Lab6/Lab6Q2.py
@@ -1,42 +1,51 @@
 import sys
 
-def func2(arr, bottom, mid, top) : 
-      
-    sm = 0
-    leftSum = -sys.maxsize - 1 #smallest possible number
-      
-    for i in range(mid, bottom-1, -1) : 
-        sm = sm + arr[i] 
-          
-        if (sm > leftSum) : 
-            leftSum = sm 
-      
-    sm = 0
-    rightSum = -sys.maxsize - 1 #smallest possible num
-    for i in range(mid + 1, top + 1) : 
-        sm = sm + arr[i] 
-          
-        if (sm > rightSum) : 
-            rightSum = sm 
-
-    return max(leftSum + rightSum, leftSum, rightSum) 
-  
-  
 def func1(arr, bottom, top) : 
       
-    if (bottom == top) : 
+    if (top == bottom) : #base case of recursion
         return arr[bottom] 
   
     mid = int((bottom + top) / 2) #mid point
   
-    retVal1 = func1(arr,bottom,top)
-    retVal2 = func1(arr, mid + 1, top)
-    retVal3 = func2(arr, bottom, mid, top)
+    retVal1 = func1(arr,bottom,mid) #recursion on list half the size
+    retVal2 = func1(arr, mid + 1, top) #recursion on list half the size
+    retVal3 = func2(arr, bottom, mid, top) 
 
     maxRetVal = max(retVal1, retVal2, retVal3)
 
     return maxRetVal 
 
-arr = [6 ,-2 , -2, 20 , -3, -2,  4]
+def func2(arr, bottom, mid, top) : 
+      
+    sum = 0
+    leftSum = -sys.maxsize - 1 #smallest possible number
+      
+    #needs to be -1 since you're moving left of the point
+    for i in range(mid, bottom-1, -1) : 
+        sum = sum + arr[i] 
+          
+        if (sum > leftSum) : 
+            leftSum = sum 
+      
+    sum = 0
+    rightSum = -sys.maxsize - 1 #smallest possible num
+    for i in range(mid + 1, top + 1) : 
+        sum = sum + arr[i] 
+          
+        if (sum > rightSum) : 
+            rightSum = sum 
 
-print(func1(arr, 0, len(arr) - 1))
+    retVal1 = leftSum
+    retVal2 = rightSum
+    retVal3 = leftSum + rightSum
+
+    maxRetVal = max(retVal1, retVal2, retVal3)
+
+    return maxRetVal 
+  
+  
+
+
+# arr = [6 ,-2 , -2, 20 , -3, -2,  4]
+
+# print(func1(arr, 0, len(arr) - 1))
